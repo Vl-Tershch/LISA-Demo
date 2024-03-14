a54713a018921e2ed67fdca003f93a635494c6b9
Tereshchenko Vlad <ddiavall@yandex.ru>
2024-03-14 20:27:02 +0300

    [feat]: added calculator

---
+++ src/calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    @classmethod
+    def add(cls, a, b):
+        return a + b
+
+    @classmethod
+    def subtract(cls, a, b):
+        return a - b
+
+    @classmethod
+    def multiply(cls, a, b):
+        return a * b
